4ff74e84951078828edc166873c95e298166956d
grynka <ihor.kozhemyakin@gmail.com>
2022-12-09 19:17:38 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,6 +10,20 @@ export class App extends Component {
     filter: ''
   };
 
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts !== null) {
+      this.setState({
+        contacts: JSON.parse(savedContacts)
+      })
+    }
+  }
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
+  }
+
   formSubmitHandler = data => {
     const contactsName = (this.state.contacts.map(contact => contact.name))
     if (contactsName.includes(data.name)) {
